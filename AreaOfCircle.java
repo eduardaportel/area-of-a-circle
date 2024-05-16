97ac4827fafa7bed7d6f684a28011c999e607213
Eduarda Portel <eduarda.portel4@gmail.com>
2024-05-16 09:21:17 -0300

    AreaOfCircle.class created

---
+++ AreaOfCircle.java
@@ -0,0 +1,15 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        // define pi
+        double pi = 3.14159;
+
+        // read the radius
+        double R = Double.parseDouble(System.console().readLine());
+
+        // define area
+        double area = pi * R * R;
+        
+        // print area
+        System.out.printf("A = %.4f \n", area);
+    } 
+}
